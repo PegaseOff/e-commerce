81923d0ee4b1fc20f32c284fdfed2bfa449204ca
Elsaay <youslisa07@gmail.com>
2026-03-03 16:24:02 +0100

    feat : l'ajout du contenu des cards, un menu

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,25 @@ import HorseList from "./Components/HorseList"
 import horsesData from "./data/horses.json"
 import {type Horse } from "./types/Horse"
 import "./Style/App.css"
+import { Menu, MenuItem, MenuLabel, MenuSeparator } from "@pegaseoff/pegase-ds"
 
 const App = () => {
   const [horses, setHorses] = useState<Horse[]>([])
-
+  const [active, setActive] = useState('home');
   useEffect(() => {
     setHorses(horsesData as Horse[])
   }, [])
 
   return (
     <div className="container">
+      <Menu activeKey={active} onChange={setActive}>
+        <MenuItem value="home">Accueil</MenuItem>
+        <MenuItem value="products">Produits</MenuItem>
+        <MenuSeparator />
+        <MenuItem value="contact">Contact</MenuItem>
+        {/* <MenuLabel>Autres</MenuLabel> */}
+        <MenuItem value="settings">Paramètres</MenuItem>
+      </Menu>
       <h1>🐎 Nos Chevaux</h1>
       <HorseList horses={horses} />
     </div>
--- src/Components/HorseCard.tsx
+++ src/Components/HorseCard.tsx
@@ -1,5 +1,5 @@
 import "../Style/HorseCard.css"
-
+import { Button, Card, CardBody, CardFooter, CardHeader, CardImage } from '@pegaseoff/pegase-ds';
 interface HorseCardProps {
   name: string
   breed: string
@@ -10,13 +10,26 @@ interface HorseCardProps {
 
 const HorseCard = ({ name, breed, origin, description, image }: HorseCardProps) => {
   return (
-    <div className="horse-card">
-      <img src={image} alt={name} />
-      <h3>{name}</h3>
-      <p><strong>Race :</strong> {breed}</p>
-      <p><strong>Origine :</strong> {origin}</p>
-      <p>{description}</p>
-    </div>
+    // <div className="horse-card">
+    //   <img src={image} alt={name} />
+    //   <h3>{name}</h3>
+    //   <p><strong>Race :</strong> {breed}</p>
+    //   <p><strong>Origine :</strong> {origin}</p>
+    //   <p>{description}</p>
+    // </div>
+    <Card size="large">
+        <CardImage src={image} alt={name} />
+        <CardHeader>
+          <h3 className="ds-card-title">{name}</h3>
+        </CardHeader>
+        <CardBody>
+          <p>{description}</p>
+        </CardBody>
+        <CardFooter>
+          <Button variant="primary" size="medium">Louer</Button>
+          <Button variant="success" size="medium">Acheter</Button>
+        </CardFooter>
+    </Card>
   )
 }
 
